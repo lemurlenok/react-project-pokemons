062c04d255632041134a6bfaa774a9175bb375b9
lemurlenok <lemurlenok@gmail.com>
2024-08-22 20:54:30 +0300

    add-5

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,35 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'; // Імплементуйте useParams
-import { PokemonServices } from '../services/api.services';
+import { useAppDispatch, useAppSelector } from '../redux/store';
+import { pokemonAction } from '../redux/slices/pokemonsSlice';
+import { useParams } from 'react-router-dom';
 import { IPokemon } from '../models/IPokemon';
+import styles from '../pages/'
 
-const PokemonDetailPage = () => {
-    const { id } = useParams(); // Отримання id з URL
+
+const PokemonDetailPage: React.FC = () => {
+    const { id } = useParams<{ id: string }>();
     const [pokemon, setPokemon] = useState<IPokemon | null>(null);
+    const dispatch = useAppDispatch();
+    const favoritePokemons = useAppSelector(state => state.PokemonSlice.favoritePokemons);
 
     useEffect(() => {
-        const fetchPokemon = async () => {
-            if (id) {
+        if (id) {
+            // Змінюється залежно від вашого API
+            const fetchPokemonDetails = async () => {
                 try {
-                    const response = await PokemonServices.getAllPokemons(0); // Використовуйте відповідний метод для отримання детальної інформації
-                    const foundPokemon = response.results.find(p => p.id === Number(id)); // Перетворюємо id на число
-                    setPokemon(foundPokemon || null);
+                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+                    const data = await response.json();
+                    setPokemon(data);
                 } catch (error) {
                     console.error('Failed to fetch Pokémon details:', error);
                 }
-            }
-        };
-
-        fetchPokemon();
+            };
+            fetchPokemonDetails();
+        }
     }, [id]);
 
+    const handleAddToFavorites = () => {
+        if (pokemon) {
+            dispatch(pokemonAction.addFavorite(pokemon));
+        }
+    };
+
     return (
-        <div>
+        <div className={styles.container}>
             {pokemon ? (
-                <div>
+                <div className={styles.pokemonDetail}>
+                    <img
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                        alt={pokemon.name}
+                        className={styles.pokemonImage}
+                    />
                     <h1>{pokemon.name}</h1>
-                    <img src={pokemon.imageUrl} alt={pokemon.name} />
-                    <p>Details about {pokemon.name}</p>
+                    <div className={styles.pokemonStats}>
+                        <h2>Abilities</h2>
+                        <ul>
+                            {pokemon.abilities.map((ability: any) => (
+                                <li key={ability.ability.name}>{ability.ability.name}</li>
+                            ))}
+                        </ul>
+                        <h2>Stats</h2>
+                        <ul>
+                            {pokemon.stats.map((stat: any) => (
+                                <li key={stat.stat.name}>
+                                    {stat.stat.name}: {stat.base_stat}
+                                </li>
+                            ))}
+                        </ul>
+                        <h2>Types</h2>
+                        <ul>
+                            {pokemon.types.map((type: any) => (
+                                <li key={type.type.name}>{type.type.name}</li>
+                            ))}
+                        </ul>
+                        <h2>Forms</h2>
+                        <ul>
+                            {pokemon.forms.map((form: any) => (
+                                <li key={form.name}>{form.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <button
+                        className={styles.favoriteButton}
+                        onClick={handleAddToFavorites}
+                    >
+                        Add to Favorites
+                    </button>
                 </div>
             ) : (
                 <p>Loading...</p>
